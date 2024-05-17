7bc28416ca3e2e76d684293f1655058bd60e72a9
Anatolii Halas <tolikgalas@gmail.com>
2024-05-17 03:05:32 +0300

    (Fixed)/ Added preventing black line

--- components/Global/Prevent.tsx
+++ components/Global/Prevent.tsx
@@ -1,7 +1,7 @@
 export default function Prevent() {
   return (
-    <div className="slide-left sticky top-0 w-full justify-center bg-black text-center text-white">
-      Not for commecial usage
+    <div className="sticky top-0 w-full justify-center bg-black text-center text-white">
+      <div className="slide-left">Not for commecial usage</div>
     </div>
   );
 }
